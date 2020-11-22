acc848f4f65aa2c16a4cdfe5b9a11bea6b6fcc41
bravacoreana <73965333+bravacoreana@users.noreply.github.com>
2020-11-23 00:38:20 +0100

    Update momentum

---
+++ js/background.js
@@ -0,0 +1,26 @@
+const body = document.querySelector("body");
+const IMG_NUMBER = 6;
+
+
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    body.appendChild(image);
+    image.classList.add('bgImage');
+}
+
+
+
+function genRandom() {
+    const number = Math.floor(Math.random() * 6);
+    return number;
+}
+
+
+function init() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
---
+++ js/weather.js
@@ -0,0 +1,60 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "bece0ff257e3fa1c84f731009f9cfab2";
+const COORDS = 'coords';
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature}℃ @ ${place}`;
+    });
+    // fetch : get data
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude: latitude,  
+        longitude: longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude);
+}
+//when the names of key and value are exactly same, 
+//it can be written like this : 
+//coordsObj = {
+//     latitude,
+//     longitude
+// }
+
+function handleGeoError() {
+    alert('weather system would not work.')
+}
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude,parsedCoords.longitude);
+        }
+
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
