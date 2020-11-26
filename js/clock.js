26ce33f52d2e409ed9d8dab14e38b215aa2f9190
bravacoreana <73965333+bravacoreana@users.noreply.github.com>
2020-11-26 21:12:32 +0100

    Create Clock face

--- js/clock.js
+++ js/clock.js
@@ -1,18 +1,28 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector(".js-clock-now");
-
+const circle = document.querySelector(".js-circle");
+const divs = document.querySelectorAll(".js-circle__clock");
 
 function getTime() {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+	const date = new Date();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+	const seconds = date.getSeconds();
 
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+	clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${
+		seconds < 10 ? `0${seconds}` : seconds
+	}`;
 
+	// paintColor;
+	for (div of divs) {
+		let numberDiv = div.id;
+		if (numberDiv <= hours) {
+			this.div.style.backgroundColor = "rgba(211, 225, 238, 0.7)";
+		}
+	}
 }
 function init() {
-    setInterval(getTime, 1000);
+	setInterval(getTime, 1000);
 }
 
 init();
