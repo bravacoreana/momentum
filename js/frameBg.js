cda2ca24ce556ed98d65a3a0f41c5d175c11b2d9
bravacoreana <bravacoreana@gmail.com>
2020-12-02 14:11:31 +0100

    Update Vertical Images

--- js/frameBg.js
+++ js/frameBg.js
@@ -1,21 +1,30 @@
 const frameBg = document.querySelector(".js-frameBg");
-const IMG_NUMBER = 6;
+const frameBgVt = document.querySelector(".js-frameBgVt");
+const IMG_NUMBER = 8;
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `images/frameHorizontal/${imgNumber + 1}.jpg`;
+  image.src = `images/frameSample/${imgNumber + 1}.jpg`;
   frameBg.appendChild(image);
   image.classList.add("bgImage");
 }
 
+function paintImageVt(imgNumber) {
+  const imageVt = new Image();
+  imageVt.src = `images/frameVertical/${imgNumber + 1}.jpg`;
+  frameBgVt.appendChild(imageVt);
+  imageVt.classList.add("bgImage");
+}
+
 function genRandom() {
-  const number = Math.floor(Math.random() * 6);
+  const number = Math.floor(Math.random() * IMG_NUMBER);
   return number;
 }
 
 function init() {
   const randomNumber = genRandom();
   paintImage(randomNumber);
+  paintImageVt(randomNumber);
 }
 
 init();
