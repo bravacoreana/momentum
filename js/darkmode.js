8b87d6da8bd2b842bc463ac2a0721b026b25625f
bravacoreana <bravacoreana@gmail.com>
2020-12-04 13:39:44 +0100

    Update details

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,8 @@
 const checkboxDM = document.querySelector("#checkbox");
+const bgImage = document.querySelectorAll(".bgImage");
 function darkmode() {
   document.querySelector(".container").classList.toggle("darkmode");
+  bgImage.forEach((bgImg) => bgImg.classList.toggle("imgDM"));
 }
 
 function init() {
