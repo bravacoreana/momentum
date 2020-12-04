112615002d7d6676680434c8c93ffd56f633105f
bravacoreana <bravacoreana@gmail.com>
2020-12-04 20:03:41 +0100

    Update darkmode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,9 +1,16 @@
 const checkboxDM = document.querySelector("#checkbox");
 const bgImage = document.querySelectorAll(".bgImage");
+const todoLists = document.querySelectorAll(".todolistDM");
+const todoListItems = document.querySelectorAll("li.item");
+
 function darkmode() {
   document.querySelector(".container").classList.toggle("darkmode");
-  document.querySelector(".frameDM").classList.toggle("DM");
-  bgImage.forEach((bgImg) => bgImg.classList.toggle("imgDM"));
+  document.querySelector(".frameDM").classList.toggle("darkmode");
+  document.querySelector(".calendarDM").classList.toggle("darkmode");
+  todoLists.forEach((todoList) => todoList.classList.toggle("darkmode"));
+  bgImage.forEach((bgImg) => bgImg.classList.toggle("darkmode"));
+  document.querySelector(".add-to-do input").classList.toggle("darkmode");
+  todoListItems.forEach((item) => item.classList.toggle("darkmode"));
 }
 
 function init() {
